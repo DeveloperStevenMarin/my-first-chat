3216e6f363afdab5284836913d4131a1e29d9421
DeveloperStevenMarin <developerstevenmarin@gmail.com>
2023-08-26 00:55:22 -0500

    frontend init

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,15 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="0894ef1f-c5c6-459c-ab7e-852fe9fd0efc"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+export default ChatsPage;
